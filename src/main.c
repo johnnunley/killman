cadabda5e27d05d59aaf73dd227c3be36aff1f6a
J.T. Nunley <xtremegamesjt@gmail.com>
2018-05-31 15:53:58 -0700

    Fixed version printing

--- src/main.c
+++ src/main.c
@@ -27,19 +27,23 @@ along with killman.  If not, see <http://www.gnu.org/licenses/>.
 char *getVersion() {
   // get the literals from the config file into memory
   char *ver = km_VERSION;
+  char *firstPeriod = ".";
   char *rev = km_REVISION;
+  char *secondPeriod = ".";
   char *mrev = km_MINOR_REVISION;
   
   // malloc the memory needed for the version string; check for errors too
-  char *fullVersion = malloc(strlen(ver) + strlen(rev) + strlen(mrev) + 1); // + 1 for the null terminator
+  char *fullVersion = malloc(strlen(ver) + strlen(firstPeriod) + strlen(rev) + strlen(secondPeriod) + strlen(mrev) + 1); // + 1 for the null terminator
   if (fullVersion == NULL) {
     // TODO: once we have an error handling system, handle the error
     exit(-1);
   }
 
-  // copy ver to the new string, then concatenate it with the rev and mrev
+  // copy ver to the new string, then concatenate it with the rev, mrev, and the periods
   strcpy(fullVersion,ver);
+  strcat(fullVersion,firstPeriod);
   strcat(fullVersion,rev);
+  strcat(fullVersion,secondPeriod);
   strcat(fullVersion,mrev);
 
   // return the new string
